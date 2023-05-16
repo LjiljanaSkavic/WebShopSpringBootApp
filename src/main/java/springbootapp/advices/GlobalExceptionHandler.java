703c2d7ba8592133b959fad0f66201a22ba4d8ca
Ljiljana <ljiljana.skavic@hotmail.com>
2023-05-16 10:47:44 +0200

    Refactor GlobalExceptionHandler

--- src/main/java/springbootapp/advices/GlobalExceptionHandler.java
+++ src/main/java/springbootapp/advices/GlobalExceptionHandler.java
@@ -1,16 +1,41 @@
 package springbootapp.advices;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import springbootapp.exceptions.NotFoundException;
+import org.springframework.web.method.HandlerMethod;
+import springbootapp.exceptions.HttpException;
+import springbootapp.util.LoggingUtil;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    
-    @ExceptionHandler(NotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public void handleNotFound() {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public final ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException e, HandlerMethod handlerMethod) {
+        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpException.class)
+    public final ResponseEntity<Object> handleHttpException(HttpException e, HandlerMethod handlerMethod) {
+        Log log = getLog(handlerMethod);
+        log.error(e);
+        if (e.getHttpStatus() == null) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(e.getData(), e.getHttpStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleException(Exception e, HandlerMethod handlerMethod) {
+        LoggingUtil.logeException(e, getLog(handlerMethod));
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private Log getLog(HandlerMethod handlerMethod) {
+        return LogFactory.getLog(handlerMethod.getMethod().getDeclaringClass());
     }
 }
