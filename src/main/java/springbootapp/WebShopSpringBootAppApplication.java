3323dba004e1fbc0196bd7f9f7f8cfc0a054ca35
Ljiljana <ljiljana.skavic@hotmail.com>
2023-05-16 15:00:01 +0200

    Add swagger dependencies

--- src/main/java/springbootapp/WebShopSpringBootAppApplication.java
+++ src/main/java/springbootapp/WebShopSpringBootAppApplication.java
@@ -4,6 +4,13 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class WebShopSpringBootAppApplication {
@@ -17,6 +24,20 @@ public class WebShopSpringBootAppApplication {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setAmbiguityIgnored(true);
         return new ModelMapper();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build().apiInfo(getApiInfo());
+
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo("Web shop app", "Web shop spring boot app built for sales and purchase", "1.0.0", "", null, "", "", new ArrayList<>());
     }
 
 }
