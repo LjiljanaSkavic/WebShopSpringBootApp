a377f86bd52bb52ad03454e67250571834223274
Ljiljana <ljiljana.skavic@hotmail.com>
2023-05-22 12:13:23 +0200

    Add shared service

--- src/main/java/springbootapp/repositories/UserEntityRepository.java
+++ src/main/java/springbootapp/repositories/UserEntityRepository.java
@@ -4,4 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import springbootapp.models.entities.UserEntity;
 
 public interface UserEntityRepository extends JpaRepository<UserEntity, Integer> {
+
+//    @Query("select u.username from UserEntity u")
+//    List<String> getAllUsernames();
+//
+//    @Query("select u from UserEntity u where u.username = :username and u.password= :password")
+//    UserEntity getUserEntityByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
 }
---
+++ src/main/java/springbootapp/services/SharedService.java
@@ -0,0 +1,26 @@
+package springbootapp.services;
+
+import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@Service
+public class SharedService {
+
+    private String createHash(String algorithm, String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance(algorithm);
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger no = new BigInteger(1, messageDigest);
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
