3ab37369f211a0de82954c7b7c9778ce806b917c
Ljiljana <ljiljana.skavic@hotmail.com>
2023-05-26 16:21:34 +0200

    Remove unnecessary code

--- src/main/java/springbootapp/models/CategoryWithParentId.java
+++ src/main/java/springbootapp/models/CategoryWithParentId.java
@@ -7,13 +7,4 @@ public class CategoryWithParentId {
     private Integer id;
     private String name;
     private Integer parentCategoryId;
-
-    public CategoryWithParentId() {
-    }
-
-    public CategoryWithParentId(Integer id, String name, Integer parentCategoryId) {
-        this.id = id;
-        this.name = name;
-        this.parentCategoryId = parentCategoryId;
-    }
 }
