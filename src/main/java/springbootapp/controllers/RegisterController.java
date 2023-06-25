30a092aef01035b79f4910c48ba577ae87f490a3
Ljiljana <ljiljana.skavic@hotmail.com>
2023-06-25 10:11:33 +0200

    Implement sending emails

--- src/main/java/springbootapp/controllers/RegisterController.java
+++ src/main/java/springbootapp/controllers/RegisterController.java
@@ -3,9 +3,11 @@ package springbootapp.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import springbootapp.exceptions.NotFoundException;
+import springbootapp.models.Email;
 import springbootapp.models.User;
 import springbootapp.models.UserRequest;
 import springbootapp.services.SharedService;
+import springbootapp.services.implementation.EmailServiceImplementation;
 import springbootapp.services.implementation.UserServiceImplementation;
 
 @CrossOrigin(origins = "http://localhost:4200/")
@@ -15,16 +17,21 @@ public class RegisterController {
 
     UserServiceImplementation userService;
     SharedService sharedService;
+    EmailServiceImplementation emailServiceImplementation;
 
-    public RegisterController(UserServiceImplementation userService, SharedService sharedService) {
+    public RegisterController(UserServiceImplementation userService, SharedService sharedService, EmailServiceImplementation emailServiceImplementation) {
         this.userService = userService;
         this.sharedService = sharedService;
+        this.emailServiceImplementation = emailServiceImplementation;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public User insert(@RequestBody UserRequest user) throws NotFoundException {
+        System.out.println(user);
         user.setActivationPin(this.sharedService.createActivationPin());
+        Email email = new Email(user.getEmail(), user.getActivationPin());
+        this.emailServiceImplementation.sendMail(email);
         return this.userService.insert(user, User.class);
     }
 
--- src/main/java/springbootapp/models/Email.java
+++ src/main/java/springbootapp/models/Email.java
@@ -6,4 +6,12 @@ import lombok.Data;
 public class Email {
     private String receiver;
     private String content;
+
+    public Email() {
+    }
+
+    public Email(String receiver, Integer content) {
+        this.receiver = receiver;
+        this.content = content.toString();
+    }
 }
