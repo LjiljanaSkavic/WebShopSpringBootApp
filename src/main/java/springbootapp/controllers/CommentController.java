43ecfb516063b16b03f3232193ffd23c64b7fd5f
Ljiljana Skavic <ljiljana.skavic@hotmail.com>
2023-12-07 19:34:44 +0100

    Fix weird bug in comment controller

--- src/main/java/springbootapp/controllers/CommentController.java
+++ src/main/java/springbootapp/controllers/CommentController.java
@@ -1,5 +1,6 @@
 package springbootapp.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import springbootapp.base.CrudController;
 import springbootapp.models.Comment;
@@ -8,6 +9,7 @@ import springbootapp.services.implementation.CommentServiceImplementation;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 @RequestMapping("/comments")
 public class CommentController extends CrudController<Integer, CommentRequest, Comment> {
@@ -19,9 +21,15 @@ public class CommentController extends CrudController<Integer, CommentRequest, C
         this.commentServiceImplementation = commentServiceImplementation;
     }
 
-    @CrossOrigin
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/product/{id}")
     public List<Comment> getCommentsForProduct(@PathVariable Integer id) {
         return commentServiceImplementation.getAllCommentsByProductId(id);
+    }
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Comment insertComment(@RequestBody CommentRequest commentRequest) {
+        return commentServiceImplementation.insert(commentRequest, Comment.class);
     }
 }
